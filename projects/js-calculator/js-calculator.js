76f34af316122da457401036fe46d47dddc5cf52
magda-gierg <mjgiergiczny@gmail.com>
2017-06-22 19:23:20 +1200

    add js-calculator

---
+++ projects/js-calculator/js-calculator.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+
+  var total = "0";
+  var isLocked = true;
+  $("#result").text(total);
+  total="";
+  $(".num_button").click(function () {
+    if ((total.length > 15)) {
+          total = total.replace(/.$/,$(this).text());
+          $("#result").text(total);
+        }
+        else{
+    total = total + ($(this).text());
+    $("#result").text(total);}
+    isLocked = true;
+  });
+
+  $(".function_button").click(function(){
+    if (isLocked === true){
+      total = total + ($(this).text());
+      $("#result").text(total);
+      isLocked = false;}
+      else {
+        total = total.replace(/.$/,$(this).text());
+        $("#result").text(total);
+      }
+    });
+
+    $("#equal_button").click(function(){
+      if (isLocked === true){
+        $("#result").text(eval(total));
+        total="";
+        isLocked = false;
+      }
+    });
+
+    $("#clearAll").click(function(){
+      total = "0";
+      $("#result").text(total);
+      total="";
+      isLocked = false;
+    });
+
+    $("#clear").click(function(){
+      total=total.slice(0, -1);
+      $("#result").text(total);
+    });
+
+    $(".function_button, #equal_button").hover(function(){
+      $(this).css('background-color','red');
+    }, function() {
+      $(this).css("background-color","#FF7F50");
+    });
+
+    $(".num_button, #clear, #clearAll ").hover(function(){
+      $(this).css('background-color','#FFE4B5');
+    }, function() {
+      $(this).css("background-color","#FFFAFA");
+    });
+
+  });
+
+    // $(".num_button").click(function () {
+    // if ((total + ($(this).text()).length > 5)) {
+    //   $("#result").text(total);
+    //       total = total.replace(/.$/,$(this).text());
+    //       $("#result").text(total);
+    //     }
+    // };
